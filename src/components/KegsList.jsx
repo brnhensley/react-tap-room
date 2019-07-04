0f653031af156e83e92287ea9faa55f8be6e1303
Brian Hensley <brnhensley@gmail.com>
2019-07-04 11:43:56 -0700

    change string props number types

--- src/components/KegsList.jsx
+++ src/components/KegsList.jsx
@@ -5,50 +5,50 @@ const allKegs = [
   {
     name: 'Sewer Brew',
     type: 'Jenkem',
-    abv: '2.5%',
-    price: '$1'
+    abv: 2.5,
+    price: 1
   },
   {
     name: 'Parsnip Wine',
     type: 'Wine',
-    abv: '8%',
-    price: '$6'
+    abv: 8,
+    price: 6
   },
   {
     name: 'Swamp Whiskey',
     type: 'Swill',
-    abv: '20%',
-    price: '$5'
+    abv: 20,
+    price: 5
   },
   {
     name: 'Teff Lager',
     type: 'Beer',
-    abv: '4%',
-    price: '$3'
+    abv: 4,
+    price: 3
   },
   {
     name: 'Honey Wine',
     type: 'Mead',
-    abv: '9%',
-    price: '$8'
+    abv: 9,
+    price: 8
   },
   {
     name: 'Pear Nectar',
     type: 'Cider',
-    abv: '2.5%',
-    price: '$1'
+    abv: 2.5,
+    price: 1
   },
   {
     name: 'Elven Goodberry Juice',
     type: 'Cider',
-    abv: '4%',
-    price: '$10'
+    abv: 4,
+    price: 10
   },
   {
     name: 'Longland Ale',
     type: 'Beer',
-    abv: '4.5%',
-    price: '$4'
+    abv: 4.5,
+    price: 4
   },
 ];
 
