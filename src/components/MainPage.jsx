765f90ca766b072ee048c21da9f74bddea275f90
Brian Hensley <brnhensley@gmail.com>
2019-07-04 15:06:33 -0700

    add UpcomingEvents comp

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Welcome from './Welcome';
-import KegsList from './KegsList';
+import UpcomingEvents from './UpcomingEvents';
 
 function MainPage(){
   return (
     <div>
-      <h3>The Starving Mule has a strict no orcs policy. All weapons and anything reeking of miasma must remain outside at your own risk. We claim no responsibility for anything stolen by kea, langurs, vultures or kobold.</h3>
+      <h3>The Starving Mule has a strict no orcs policy, and we are currently at capacity for bards, try the Rusty Anvil. All weapons and anything reeking of miasma must remain outside at your own risk. We claim no responsibility for anything stolen by kea, langurs, vultures or kobold.</h3>
       <Welcome />
+      <UpcomingEvents />
     </div>
   );
 }
---
+++ src/components/UpcomingEvents.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function UpcomingEvents(){
+  return (
+    <div>
+    <style jsx>{`
+        span {
+          background-color: red;
+        }
+      `}</style>
+      <h3><em>8-4-19</em>: 31st annual Gnome toss - B.Y.O.G.</h3>
+      <h3><em>8-16-19</em>: Come laugh at the elven traders and their stupid wooden weapons.</h3>
+      <h3><em>9-1-19</em>: <span>Stay Away - The goblins show up the beginning of every September.</span></h3>
+      <h3><em>10-1-19</em>: Goblins should be gone by now... show up at your own risk, doors open if it's safe.</h3>
+    </div>
+  );
+}
+
+export default UpcomingEvents;
