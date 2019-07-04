ef57f69378b0659a6d7d6ed2529577adcf8b445d
Brian Hensley <brnhensley@gmail.com>
2019-07-04 16:29:07 -0700

    made Welcome comp into KegsLink comp

--- src/components/KegsLink.jsx
+++ src/components/KegsLink.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-function Welcome(){
+function KegsLink(){
   return (
     <div>
       <h1><Link to="/kegs">Click here to see what drinks are in rotation on {today}.</Link></h1>
@@ -15,4 +15,4 @@ let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'A
 let date = new Date();
 let today = (months[date.getMonth()] + ' ' + date.getDay() + ', ' + date.getFullYear());
 
-export default Welcome;
+export default KegsLink;
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import Welcome from './Welcome';
-import UpcomingEvents from './UpcomingEvents';
+import KegsLink from './KegsLink';
+import UpcomingEventsHeader from './UpcomingEventsHeader.jsx';
 
 function MainPage(){
   return (
     <div>
       <h3>The Starving Mule has a strict no orcs policy, and we are currently at capacity for bards, try the Rusty Anvil. All weapons and anything reeking of miasma must remain outside at your own risk. We claim no responsibility for anything stolen by kea, langurs, vultures or kobold.</h3>
-      <Welcome />
-      <UpcomingEvents />
+      <KegsLink />
+      <UpcomingEventsHeader />
     </div>
   );
 }
