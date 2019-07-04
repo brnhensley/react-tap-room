29baecc5e0dd81a2693e3fc8a897cee82066dd06
Brian Hensley <brnhensley@gmail.com>
2019-07-04 11:42:37 -0700

    add keg  display info to loop

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Keg() {
+function Keg(props) {
   return (
     <div>
-      <h2>I an a Keg</h2>
-
+      <h2>{props.name}</h2>
+      <h3>Type: {props.type} </h3>
+      <h3>Alcohol by Volume: {props.abv}% </h3>
+      <h3>Price per Pint: ${props.price} </h3>
+      <hr/>
     </div>
   );
 
 }
+
+Keg.PropTypes = {
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  abv: PropTypes.number.isRequired,
+  price: PropTypes.number.isRequired,
+};
 
 export default Keg;
