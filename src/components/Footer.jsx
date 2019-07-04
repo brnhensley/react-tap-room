e156e3e96d98cb245097a584bd354eed64538317
Brian Hensley <brnhensley@gmail.com>
2019-07-04 14:49:32 -0700

    add footer content and style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,29 @@ import React from 'react';
 
 function Footer(){
   return (
-    <div>
-      <h1>I an a footer</h1>
-    </div>
+    <footer>
+      <style jsx>{`
+        footer {
+        background-color: lightgrey;
+        width: 100%;
+        border-top: 4px solid black;
+        margin-top: 2%;
+        padding-top: 2%;
+        position: relative;
+        left: 0;
+        bottom: 0;
+      }
+      h5 {
+        margin: 2%;
+        padding: 2%;
+      }
+      `}</style>
+
+      <h5> Stay away from the mule, it is starving because of a werecurse. The mule is crazed for blood and flesh so we will not come to your rescue.  That mule is dead to us even if it lives forever.</h5>
+    </footer>
   );
 }
 
 export default Footer;
+
+// Find out why the footer won't stretch side all 3 edges
