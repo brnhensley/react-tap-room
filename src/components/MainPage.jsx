e79262b1c399b609b54add254d4e2633d8d1f609
Brian Hensley <brnhensley@gmail.com>
2019-07-04 10:15:27 -0700

    create MainPage comp

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import MainHeader from './MainHeader';
+import KegsList from './KegsList';
 
 function MainPage(){
   return (
     <div>
       <h1>I an a main page</h1>
+      <MainHeader />
+      <KegsList />
     </div>
   );
 }
