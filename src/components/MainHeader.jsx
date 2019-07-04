47e901927166fa94112beabb38cb60b68db6721a
Brian Hensley <brnhensley@gmail.com>
2019-07-04 13:59:18 -0700

    add a current date

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 function MainHeader(){
   return (
     <div>
-      <h1>I an a main header</h1>
+      <h1>Click here to see what's in rotation on {today}.</h1>
     </div>
   );
 }
+
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+let date = new Date();
+let today = (months[date.getMonth()] + ' ' + date.getDay() + ', ' + date.getFullYear());
 
 export default MainHeader;
