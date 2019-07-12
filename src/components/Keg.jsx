2b164bcdaeb82ce5a64caccf9a6378aa8aaae985
Brian Hensley <brnhensley@gmail.com>
2019-07-12 10:13:17 -0700

    add if conditional for Unicorn blood ABV

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -2,28 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Keg(props) {
+  let display = `Alcohol by Volume: ${props.abv}`;
+  if (props.abv == 0) {
+    display = 'You\'re not drinking this for its alcohol';
+  }
+
   return (
     <div>
-      <style jsx>{`
-      div {
-        background-color: skyblue;
-        width: 80%;
-        border: 4px solid black;
-        margin: 2%;
-        padding: 2%;
-        border-radius: 25px;
-        margin-left: auto;
-        margin-right: auto;
-      }
-      `}</style>
+      <style jsx>
+        {`
+          div {
+            background-color: skyblue;
+            width: 80%;
+            border: 4px solid black;
+            margin: 2%;
+            padding: 2%;
+            border-radius: 25px;
+            margin-left: auto;
+            margin-right: auto;
+          }
+        `}
+      </style>
       <h2>{props.name}</h2>
       <h3>Type: {props.type} </h3>
-      <h3>Alcohol by Volume: {props.abv}% </h3>
+      <h3>{display}</h3>
       <h3>Price per Pint: ${props.price} </h3>
       <h4>{props.description}</h4>
     </div>
   );
-
 }
 
 Keg.propTypes = {
