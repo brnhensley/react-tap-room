d76b587578909108ff23da2ed9f96f3eb11581e3
Brian Hensley <brnhensley@gmail.com>
2019-07-12 11:53:16 -0700

    finish new keg form

--- src/components/NewKegForm.jsx
+++ src/components/NewKegForm.jsx
@@ -6,12 +6,16 @@ function NewKegForm(props) {
   let _type = null;
   let _abv = null;
   let _price = null;
-  let _pints = 124;
   let _description = null;
 
   function handleNewKegSubmission(event) {
     event.preventDefault();
-    props.onNewKegCreation;
+    props.onNewKegCreation({name: _name.value, type: _type.value, abv: _abv.value, price: _price.value, pints: 124, description: _description.value});
+    _name.value = null;
+    _type.value = null;
+    _abv.value = null;
+    _price.value = null;
+    _description.value = null;
   }
 
   return (
@@ -27,20 +31,24 @@ function NewKegForm(props) {
           type='text'
           id='type'
           placeholder='Drink Type'
+          ref={(input) => {_type=input;}}
         />
         <input
           type='number'
           id='abv'
           placeholder='ABV'
+          ref={(input) => {_abv=input;}}
         />
         <input
           type='number'
           id='price'
           placeholder='Price per pint'
+          ref={(input) => {_price=input;}}
         />
         <textarea
           id='description'
           placeholder='Description'
+          ref={(input) => {_description=input;}}
         />
         <button type='submit'>Add Keg!</button>
       </form>
