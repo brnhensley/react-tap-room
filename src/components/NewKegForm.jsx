4ff79f8ad63db70e487e66d14edc6cdedb0f70e3
Brian Hensley <brnhensley@gmail.com>
2019-07-12 12:51:12 -0700

    style keg form

--- src/components/NewKegForm.jsx
+++ src/components/NewKegForm.jsx
@@ -20,36 +20,52 @@ function NewKegForm(props) {
 
   return (
     <div>
+      <style jsx>
+        {`
+          div {
+            background-color: salmon;
+            width: 40%;
+            border: 4px solid black;
+            margin: 2%;
+            padding: 2%;
+            border-radius: 25px;
+            margin-left: auto;
+            margin-right: auto;
+            text-align: center;
+          }
+        `}
+      </style>
       <form onSubmit={handleNewKegSubmission}>
+        <h3>Add a new keg:</h3>
         <input
           type='text'
           id='name'
           placeholder='Keg Name'
           ref={(input) => {_name=input;}}
-        />
+        /><br/>
         <input
           type='text'
           id='type'
           placeholder='Drink Type'
           ref={(input) => {_type=input;}}
-        />
+        /><br/>
         <input
           type='number'
           id='abv'
           placeholder='ABV'
           ref={(input) => {_abv=input;}}
-        />
+        /><br/>
         <input
           type='number'
           id='price'
           placeholder='Price per pint'
           ref={(input) => {_price=input;}}
-        />
+        /><br/>
         <textarea
           id='description'
           placeholder='Description'
           ref={(input) => {_description=input;}}
-        />
+        /><br/>
         <button type='submit'>Add Keg!</button>
       </form>
     </div>
